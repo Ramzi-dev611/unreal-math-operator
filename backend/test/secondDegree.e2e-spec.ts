c9b031fd66efaccdc8d196657ed7210692768cce
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-06-07 19:07:06 +0100

    fixes in tests scripts 2

--- backend/test/secondDegree.e2e-spec.ts
+++ backend/test/secondDegree.e2e-spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { AppController } from '../src/main/app.controller';
+import { AppService } from '../src/main/app.service';
 
 describe('Second degree resolver end point test', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
